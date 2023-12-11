767fbd73116fcb209896722eec2646914a6721b1
igentuman <igentuman@gmail.com>
2023-12-12 00:25:12 +0100

    placed features filter disable

--- src/main/java/igentuman/nc/mixin/ForgeHooksMixin.java
+++ src/main/java/igentuman/nc/mixin/ForgeHooksMixin.java
@@ -1,13 +1,11 @@
 package igentuman.nc.mixin;
 
 import igentuman.nc.content.materials.Ores;
-import net.minecraft.resources.RegistryResourceAccess;
 import net.minecraft.resources.ResourceKey;
 import net.minecraftforge.common.ForgeHooks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -19,7 +17,7 @@ import static igentuman.nc.NuclearCraft.MODID;
 @Mixin(ForgeHooks.class)
 public class ForgeHooksMixin {
 
-    @Inject(method = "filterThunks", at = @At("TAIL"), remap = false, cancellable = true)
+/*    @Inject(method = "filterThunks", at = @At("TAIL"), remap = false, cancellable = true)
     private static void filterThunks(Map<ResourceKey<?>, RegistryResourceAccess.EntryThunk<?>> map, CallbackInfoReturnable<Collection<Map.Entry<ResourceKey<?>, RegistryResourceAccess.EntryThunk<?>>>> cir) {
         Map<ResourceKey<?>, RegistryResourceAccess.EntryThunk<?>> map1 = new HashMap<>();
         for(Map.Entry<ResourceKey<?>, RegistryResourceAccess.EntryThunk<?>> entry : cir.getReturnValue()) {
@@ -35,5 +33,5 @@ public class ForgeHooksMixin {
             }
         }
         cir.setReturnValue(map1.entrySet());
-    }
+    }*/
 }

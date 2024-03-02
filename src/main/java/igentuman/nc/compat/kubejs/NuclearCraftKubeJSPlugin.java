48ef3d0b8a1d94c1f311a196007e09365e5c2b53
igentuman <igentuman@gmail.com>
2024-03-02 01:43:40 +0100

    Kubejs

--- src/main/java/igentuman/nc/compat/kubejs/NuclearCraftKubeJSPlugin.java
+++ src/main/java/igentuman/nc/compat/kubejs/NuclearCraftKubeJSPlugin.java
@@ -7,11 +7,19 @@ import dev.latvian.mods.kubejs.fluid.OutputFluid;
 import dev.latvian.mods.kubejs.item.InputItem;
 import dev.latvian.mods.kubejs.item.OutputItem;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
+import dev.latvian.mods.kubejs.recipe.RecipesEventJS;
 import dev.latvian.mods.kubejs.recipe.component.FluidComponents;
 import dev.latvian.mods.kubejs.recipe.component.ItemComponents;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
+import igentuman.nc.block.entity.fission.FissionControllerBE;
+import igentuman.nc.block.entity.turbine.TurbineControllerBE;
 import igentuman.nc.recipes.NcRecipeType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeManager;
+
+import java.util.Map;
 
 import static igentuman.nc.NuclearCraft.MODID;
 
@@ -22,6 +30,14 @@ public class NuclearCraftKubeJSPlugin extends KubeJSPlugin {
 
     @Override
     public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
+        event.namespace(MODID).register(FissionControllerBE.NAME, SCHEMA);
+        event.namespace(MODID).register(TurbineControllerBE.NAME, SCHEMA);
+        event.namespace(MODID).register("nc_ore_veins", SCHEMA);
+        event.namespace(MODID).register("fusion_core", SCHEMA);
+        event.namespace(MODID).register("fusion_coolant", SCHEMA);
+        event.namespace(MODID).register("fission_boiling", SCHEMA);
+        event.namespace(MODID).special("reset_nbt");
+        event.namespace(MODID).special("shielding");
         for(String recipeType: NcRecipeType.ALL_RECIPES.keySet()) {
             event.namespace(MODID).register(recipeType, SCHEMA);
         }

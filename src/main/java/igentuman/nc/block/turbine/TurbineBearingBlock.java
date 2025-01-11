e5720d0da746691e091f527e2fc1da9dbe6246a5
igentuman <igentuman@gmail.com>
2025-01-12 00:12:34 +0100

    Turbine Bearing crash #157

--- src/main/java/igentuman/nc/block/turbine/TurbineBearingBlock.java
+++ src/main/java/igentuman/nc/block/turbine/TurbineBearingBlock.java
@@ -1,5 +1,6 @@
 package igentuman.nc.block.turbine;
 
+import igentuman.nc.block.MultiblockBlock;
 import igentuman.nc.block.entity.turbine.TurbineBE;
 import igentuman.nc.block.entity.turbine.TurbineBladeBE;
 import igentuman.nc.util.TextUtils;
@@ -32,43 +33,10 @@ import java.util.Objects;
 
 import static igentuman.nc.multiblock.turbine.TurbineRegistration.TURBINE_BE;
 
-public class TurbineBearingBlock extends Block implements EntityBlock {
+public class TurbineBearingBlock extends MultiblockBlock {
 
     public TurbineBearingBlock(Properties pProperties) {
         super(pProperties);
-    }
-
-    private String codeID()
-    {
-        return ForgeRegistries.BLOCKS.getKey(this).getPath();
-    }
-
-    @Nullable
-    @Override
-    public BlockEntity newBlockEntity(@NotNull BlockPos pPos, @NotNull BlockState pState) {
-        return TURBINE_BE.get("turbine_bearing").get().create(pPos, pState);
-    }
-
-    @javax.annotation.Nullable
-    @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> type) {
-        if (level.isClientSide()) {
-            return (lvl, pos, blockState, t) -> {
-                if (t instanceof TurbineBladeBE tile) {
-                    tile.tickClient();
-                }
-            };
-        }
-        return (lvl, pos, blockState, t)-> {
-            if (t instanceof TurbineBladeBE tile) {
-                tile.tickServer();
-            }
-        };
-    }
-
-    @Override
-    public void onNeighborChange(BlockState state, LevelReader level, BlockPos pos, BlockPos neighbor){
-        ((TurbineBE) Objects.requireNonNull(level.getBlockEntity(pos))).onNeighborChange(state,  pos, neighbor);
     }
 
     @Override
